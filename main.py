a4bf5748244af45217c03b3c598f2841dd8af5ac
VaradRanade <varadranade1098@gmail.com>
2026-08-24 11:45:09 +0530

    Updated Birthday Wisher Code
    Removed unnecessary setup instructions and cleaned up code.

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
 import pandas as pd
 import datetime as dt
 import random
@@ -40,8 +32,3 @@ for index, row in data.iterrows():
             )
 
         print(f"Birthday email successfully sent to {row['name']}!")
-
-
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )

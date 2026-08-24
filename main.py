0000b3dc7731c798bdb3198bd22c2b37d557369d
VaradRanade <varadranade1098@gmail.com>
2026-08-24 11:41:40 +0530

    Updated Birthday Wisher Code

--- main.py
+++ main.py
@@ -16,18 +16,13 @@ my_email = os.environ.get("MY_EMAIL")
 password = os.environ.get("MY_PASSWORD")
 
 data = pd.read_csv("birthdays.csv")
-month = data["month"].head()
-day = data["day"].head()
-
 
 now = dt.datetime.now()
 today_day = now.day
 today_month = now.month
 
-
 for index, row in data.iterrows():
     if row["month"] == today_month and row["day"] == today_day:
-        
 
         random_letter_file = f"letter_templates/letter_{random.randint(1, 3)}.txt"
 
@@ -35,13 +30,14 @@ for index, row in data.iterrows():
             letter_contents = letter_file.read()
             personalized_letter = letter_contents.replace("[NAME]", row["name"])
 
-        
-
-
-        with smtplib.SMTP("smtp.gmail.com",587) as connection :
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
-            connection.login(user=my_email , password=password)
-            connection.sendmail(from_addr=my_email , to_addrs= row["email"] , msg=f"Subject:Happy Birthday!\n\n{personalized_letter}")
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=row["email"],
+                msg=f"Subject: Happy Birthday!\n\n{personalized_letter}"
+            )
 
         print(f"Birthday email successfully sent to {row['name']}!")
 
